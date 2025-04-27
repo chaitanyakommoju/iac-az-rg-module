ee4b16efe4ef96d48083f4db07e0824189ed5a8b
chaitanyak <navchaitanya@gmail.com>
2025-04-28 01:48:09 +0530

    Added initial IAC code

--- main.tf
+++ main.tf
@@ -12,11 +12,3 @@ resource "azurerm_resource_group" "this" {
     }
   )
 }
-
-output "resource_group_name" {
-  value = azurerm_resource_group.this.name
-}
-
-output "resource_group_id" {
-  value = azurerm_resource_group.this.id
-}
